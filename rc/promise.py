feaf815f0f9e3a8feca72fbf12eddf67661fab02
Shipeng Feng <fsp261@gmail.com>
2015-11-17 14:56:32 +0800

    Use value instead of result for promise

--- rc/promise.py
+++ rc/promise.py
@@ -3,22 +3,22 @@ RESOLVED_STATE = 1
 
 
 class Promise(object):
-    """A promise object.  You can access ``promise.result`` to get the
+    """A promise object.  You can access ``promise.value`` to get the
     resolved value.  Here is one example::
 
         p = Promise()
         assert p.is_pending
         assert not p.is_resolved
-        assert p.result is None
+        assert p.value is None
         p.resolve('value')
         assert not p.is_pending
         assert p.is_resolved
-        assert p.result == 'value'
+        assert p.value == 'value'
     """
 
     def __init__(self):
         #: the value for this promise if it's resolved
-        self.result = None
+        self.value = None
         self._state = PENDING_STATE
         self._callbacks = []
 
@@ -26,7 +26,7 @@ class Promise(object):
         """Resolves with value."""
         if self._state != PENDING_STATE:
             raise RuntimeError('Promise is no longer pending.')
-        self.result = value
+        self.value = value
         self._state = RESOLVED_STATE
         for callback in self._callbacks:
             callback(value)
@@ -49,21 +49,21 @@ class Promise(object):
             d = {}
             p.then(lambda v: d.setdefault('key', v))
             p.resolve('value')
-            assert p.result == 'value'
+            assert p.value == 'value'
             assert d['key'] == 'value'
         """
         if on_resolve is not None:
             if self._state == PENDING_STATE:
                 self._callbacks.append(on_resolve)
             elif self._state == RESOLVED_STATE:
-                on_resolve(self.result)
+                on_resolve(self.value)
         return self
 
     def __repr__(self):
         if self._state == PENDING_STATE:
             v = '(pending)'
         else:
-            v = repr(self.result)
+            v = repr(self.value)
         return '<%s %s>' % (
             self.__class__.__name__,
             v,
--- tests/test_promise.py
+++ tests/test_promise.py
@@ -7,11 +7,11 @@ def test_promise_resolve():
     p = Promise()
     assert p.is_pending
     assert not p.is_resolved
-    assert p.result is None
+    assert p.value is None
     p.resolve('value')
     assert not p.is_pending
     assert p.is_resolved
-    assert p.result == 'value'
+    assert p.value == 'value'
     with pytest.raises(RuntimeError):
         p.resolve('value again')
 
@@ -21,5 +21,5 @@ def test_promise_then():
     d = {}
     v = p.then(lambda v: d.setdefault('key', v))
     p.resolve('value')
-    assert v.result == 'value'
+    assert v.value == 'value'
     assert d['key'] == 'value'
