ee2ad09b60daa9abd0209829dbe5d6d3adb477e8
fatelei <fatelei@gmail.com>
2016-06-22 12:23:26 +0800

    [Fix] fix ketama get node when key is unicode.

--- tests/test_ketama.py
+++ tests/test_ketama.py
@@ -4,7 +4,12 @@ from rc.ketama import HashRing
 def test_basic():
     nodes = ['node01', 'node02', 'node03', 'node04']
     hashring = HashRing(nodes)
-    keys = ['key-%s' % i for i in range(1000)]
+    keys = [u'key-%s' % i for i in range(500)]
+    keys_nodes = [hashring.get_node(k) for k in keys]
+    for node in nodes:
+        assert node in keys_nodes
+
+    keys = ['key-%s' % i for i in range(500, 1000)]
     keys_nodes = [hashring.get_node(k) for k in keys]
     for node in nodes:
         assert node in keys_nodes
